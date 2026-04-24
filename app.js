a65675c9127bb6276c437965c256b8cea3512ec3
Aditi Dubey <aditidubey1105@gmail.com>
2026-04-24 12:28:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
 // ============================================
 // ASSESSMENT DATA
 // ============================================
-const SUPABASE_URL = "https://jgozwnygkuuxkwxhrhqk.supabase.co";
-const SUPABASE_KEY = "sb_publishable_nF2FaubTOihhXqSYyETQzA_iv5huqqH";
+const SUPABASE_URL = "https://bphdfjtllukteqicztvo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_M8sfW8rk5_pswRdG-16Wbg_Rc56zRfB";
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const TESTS = [
   {
