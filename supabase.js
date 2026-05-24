ef561a00114e8672aa2a963c205880579aa1bd12
Aditi Dubey <aditidubey1105@gmail.com>
2026-05-24 18:54:39 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -5,7 +5,7 @@
 const SUPABASE_URL = "https://jgozwnygkuuxkwxhrhqk.supabase.co"; // ← Correct URL
 const SUPABASE_KEY = "sb_publishable_nF2FaubTOihhXqSYyETQzA_iv5huqqH";
 
-const _supabase = _supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Make it globally available
 window._supabase = _supabase;
